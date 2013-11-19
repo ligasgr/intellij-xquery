7bce67f87f1fa9b3ed0873ff508b9580307ebcfd
ligasgr <ligasgr@gmail.com>
2013-11-19 22:57:11 +0000

    Extract variables panel from main tab.

--- src/main/java/org/intellij/xquery/runner/ui/run/main/RunConfigurationMainTab.java
+++ src/main/java/org/intellij/xquery/runner/ui/run/main/RunConfigurationMainTab.java
@@ -16,24 +16,14 @@
 
 package org.intellij.xquery.runner.ui.run.main;
 
-import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.options.SettingsEditor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.LabeledComponent;
-import com.intellij.ui.AnActionButton;
-import com.intellij.ui.AnActionButtonRunnable;
-import com.intellij.ui.AnActionButtonUpdater;
 import com.intellij.ui.PanelWithAnchor;
-import com.intellij.ui.TableUtil;
-import com.intellij.ui.ToolbarDecorator;
-import com.intellij.ui.table.TableView;
-import com.intellij.util.ui.ColumnInfo;
-import com.intellij.util.ui.ListTableModel;
 import com.intellij.util.ui.UIUtil;
 import org.intellij.xquery.runner.state.datasources.XQueryDataSourcesSettings;
 import org.intellij.xquery.runner.state.run.XQueryRunConfiguration;
-import org.intellij.xquery.runner.state.run.XQueryRunVariable;
 import org.intellij.xquery.runner.state.run.XQueryRunVariables;
 import org.intellij.xquery.runner.ui.datasources.DataSourceDetailsPanel;
 import org.intellij.xquery.runner.ui.datasources.DataSourceListPanel;
@@ -41,13 +31,6 @@ import org.intellij.xquery.runner.ui.datasources.DataSourcesSettingsForm;
 import org.intellij.xquery.runner.ui.run.main.datasource.DataSourceSelector;
 import org.intellij.xquery.runner.ui.run.main.datasource.DataSourcesDialog;
 import org.intellij.xquery.runner.ui.run.main.module.XQueryModuleSelector;
-import org.intellij.xquery.runner.ui.run.main.variables.IsActiveColumnInfo;
-import org.intellij.xquery.runner.ui.run.main.variables.NameColumnInfo;
-import org.intellij.xquery.runner.ui.run.main.variables.NamespaceColumnInfo;
-import org.intellij.xquery.runner.ui.run.main.variables.TypeColumnInfo;
-import org.intellij.xquery.runner.ui.run.main.variables.ValueColumnInfo;
-import org.intellij.xquery.runner.ui.run.main.variables.VariableDialog;
-import org.intellij.xquery.runner.ui.run.main.variables.VariableDialogWrapper;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -55,11 +38,8 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
-import java.awt.BorderLayout;
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 
 /**
  * User: ligasgr
@@ -68,9 +48,7 @@ import java.util.ArrayList;
  */
 public class RunConfigurationMainTab extends SettingsEditor<XQueryRunConfiguration> implements PanelWithAnchor {
 
-    private JPanel variablesPanel;
-    private final TableView<XQueryRunVariable> variablesTable;
-    private final ListTableModel<XQueryRunVariable> variablesModel;
+    private VariablesPanel variablesPanel;
     private LabeledComponent<XQueryModuleSelector> mainFile;
     private ContextItemPanel contextItemPanel;
     private final Project project;
@@ -79,19 +57,9 @@ public class RunConfigurationMainTab extends SettingsEditor<XQueryRunConfigurati
     private JButton configureDataSourcesButton;
     private LabeledComponent<JComboBox> dataSourceSelectorComponent;
     private DataSourceSelector dataSourceSelector;
-    private final ColumnInfo<XQueryRunVariable, String> NAME = new NameColumnInfo();
-    private final ColumnInfo<XQueryRunVariable, String> NAMESPACE = new NamespaceColumnInfo();
-    private final ColumnInfo<XQueryRunVariable, String> TYPE = new TypeColumnInfo();
-    private final ColumnInfo<XQueryRunVariable, String> VALUE = new ValueColumnInfo();
-    private final ColumnInfo<XQueryRunVariable, Boolean> IS_ACTIVE = new IsActiveColumnInfo();
 
     public RunConfigurationMainTab(final Project project) {
         this.project = project;
-        variablesModel = new ListTableModel<XQueryRunVariable>(IS_ACTIVE, NAME, NAMESPACE, TYPE, VALUE);
-        variablesTable = prepareVariablesTable();
-        ToolbarDecorator variablesTableToolbarDecorator = prepareVariablesTableToolbarDecorator(variablesTable);
-        variablesPanel.add(variablesTableToolbarDecorator.createPanel(), BorderLayout.CENTER);
-        variablesPanel.setPreferredSize(new Dimension(- 1, 120));
         dataSourceSelector = new DataSourceSelector(dataSourceSelectorComponent.getComponent());
         configureDataSourcesButton.addActionListener(new ActionListener() {
             @Override
@@ -105,80 +73,7 @@ public class RunConfigurationMainTab extends SettingsEditor<XQueryRunConfigurati
             }
         });
 
-        anchor = UIUtil.mergeComponentsWithAnchor(mainFile, contextItemPanel);
-    }
-
-    private ToolbarDecorator prepareVariablesTableToolbarDecorator(final TableView<XQueryRunVariable> variablesTable) {
-        return ToolbarDecorator.createDecorator(variablesTable)
-                .setAddAction(new AnActionButtonRunnable() {
-                    @Override
-                    public void run(AnActionButton button) {
-                        ArrayList<XQueryRunVariable> newList = new ArrayList<XQueryRunVariable>
-                                (variablesModel.getItems());
-                        XQueryRunVariable newVariable = new XQueryRunVariable();
-                        if (showEditorDialog(newVariable)) {
-                            newList.add(newVariable);
-                            variablesModel.setItems(newList);
-                            int index = variablesModel.getRowCount() - 1;
-                            variablesModel.fireTableRowsInserted(index, index);
-                            variablesTable.setRowSelectionInterval(index, index);
-                        }
-                    }
-                }).setRemoveAction(new AnActionButtonRunnable() {
-                    @Override
-                    public void run(AnActionButton button) {
-                        TableUtil.stopEditing(variablesTable);
-                        final int[] selected = variablesTable.getSelectedRows();
-                        if (selected == null || selected.length == 0) return;
-                        for (int i = selected.length - 1; i >= 0; i--) {
-                            variablesModel.removeRow(selected[i]);
-                        }
-                        for (int i = selected.length - 1; i >= 0; i--) {
-                            int idx = selected[i];
-                            variablesModel.fireTableRowsDeleted(idx, idx);
-                        }
-                        int selection = selected[0];
-                        if (selection >= variablesModel.getRowCount()) {
-                            selection = variablesModel.getRowCount() - 1;
-                        }
-                        if (selection >= 0) {
-                            variablesTable.setRowSelectionInterval(selection, selection);
-                        }
-                        variablesTable.requestFocus();
-                    }
-                }).setEditAction(new AnActionButtonRunnable() {
-                    @Override
-                    public void run(AnActionButton button) {
-                        final int selectedRow = variablesTable.getSelectedRow();
-                        final XQueryRunVariable selectedVariable = variablesTable.getSelectedObject();
-                        showEditorDialog(selectedVariable);
-                        variablesModel.fireTableDataChanged();
-                        variablesTable.setRowSelectionInterval(selectedRow, selectedRow);
-                    }
-                }).setRemoveActionUpdater(new AnActionButtonUpdater() {
-                    @Override
-                    public boolean isEnabled(AnActionEvent e) {
-                        return variablesTable.getSelectedRowCount() >= 1;
-                    }
-                }).setEditActionUpdater(new AnActionButtonUpdater() {
-                    @Override
-                    public boolean isEnabled(AnActionEvent e) {
-                        return variablesTable.getSelectedRowCount() >= 1 &&
-                                variablesTable.getSelectedObject() != null;
-                    }
-                }).disableUpDownActions();
-    }
-
-    private TableView<XQueryRunVariable> prepareVariablesTable() {
-        TableView<XQueryRunVariable> variablesTable = new TableView<XQueryRunVariable>(variablesModel);
-        variablesTable.getEmptyText().setText("No variables defined");
-        variablesTable.setColumnSelectionAllowed(false);
-        variablesTable.setShowGrid(false);
-        variablesTable.setDragEnabled(false);
-        variablesTable.setShowHorizontalLines(false);
-        variablesTable.setShowVerticalLines(false);
-        variablesTable.setIntercellSpacing(new Dimension(0, 0));
-        return variablesTable;
+        anchor = UIUtil.mergeComponentsWithAnchor(mainFile, contextItemPanel, variablesPanel);
     }
 
     @Override
@@ -190,6 +85,7 @@ public class RunConfigurationMainTab extends SettingsEditor<XQueryRunConfigurati
     public void setAnchor(@Nullable JComponent anchor) {
         mainFile.setAnchor(anchor);
         contextItemPanel.setAnchor(anchor);
+        variablesPanel.setAnchor(anchor);
     }
 
     public XQueryModuleSelector getMainFileField() {
@@ -199,9 +95,7 @@ public class RunConfigurationMainTab extends SettingsEditor<XQueryRunConfigurati
     @Override
     protected void resetEditorFrom(XQueryRunConfiguration configuration) {
         getMainFileField().setText(configuration.getMainFileName());
-        ArrayList<XQueryRunVariable> newList = new ArrayList<XQueryRunVariable>(configuration.getVariables()
-                .getVariables());
-        variablesModel.setItems(newList);
+        variablesPanel.setVariables(configuration.getVariables());
         contextItemPanel.init(configuration.isContextItemEnabled(), configuration.getContextItemText(),
                 configuration.getContextItemFile(), configuration.isContextItemFromEditorEnabled(),
                 configuration.getContextItemType());
@@ -211,7 +105,7 @@ public class RunConfigurationMainTab extends SettingsEditor<XQueryRunConfigurati
     @Override
     protected void applyEditorTo(XQueryRunConfiguration configuration) throws ConfigurationException {
         configuration.setMainFileName(getMainFileField().getText());
-        configuration.setVariables(new XQueryRunVariables(variablesModel.getItems()));
+        configuration.setVariables(new XQueryRunVariables(variablesPanel.getVariables()));
         configuration.setContextItemEnabled(contextItemPanel.isContextItemEnabled());
         configuration.setContextItemFile(contextItemPanel.getContextItemPath());
         configuration.setContextItemText(contextItemPanel.getContextItemEditorContent());
@@ -234,16 +128,5 @@ public class RunConfigurationMainTab extends SettingsEditor<XQueryRunConfigurati
         mainFile = new LabeledComponent<XQueryModuleSelector>();
         mainFile.setComponent(new XQueryModuleSelector(project));
         contextItemPanel = new ContextItemPanel(project);
-    }
-
-    private static boolean showEditorDialog(@NotNull XQueryRunVariable variable) {
-        VariableDialogWrapper wrapper = new VariableDialogWrapper(new VariableDialog());
-        wrapper.init(variable);
-        wrapper.show();
-        if (wrapper.isOK()) {
-            wrapper.applyValuesTo(variable);
-            return true;
-        }
-        return false;
     }
 }
---
+++ src/main/resources/org/intellij/xquery/runner/ui/run/main/VariablesPanel.java
@@ -0,0 +1,175 @@
+/*
+ * Copyright 2013 Grzegorz Ligas <ligasgr@gmail.com> and other contributors (see the CONTRIBUTORS file).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.intellij.xquery.runner.ui.run.main;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.ui.AnActionButton;
+import com.intellij.ui.AnActionButtonRunnable;
+import com.intellij.ui.AnActionButtonUpdater;
+import com.intellij.ui.PanelWithAnchor;
+import com.intellij.ui.TableUtil;
+import com.intellij.ui.ToolbarDecorator;
+import com.intellij.ui.table.TableView;
+import com.intellij.util.ui.ColumnInfo;
+import com.intellij.util.ui.ListTableModel;
+import org.intellij.xquery.runner.state.run.XQueryRunVariable;
+import org.intellij.xquery.runner.state.run.XQueryRunVariables;
+import org.intellij.xquery.runner.ui.run.main.variables.IsActiveColumnInfo;
+import org.intellij.xquery.runner.ui.run.main.variables.NameColumnInfo;
+import org.intellij.xquery.runner.ui.run.main.variables.NamespaceColumnInfo;
+import org.intellij.xquery.runner.ui.run.main.variables.TypeColumnInfo;
+import org.intellij.xquery.runner.ui.run.main.variables.ValueColumnInfo;
+import org.intellij.xquery.runner.ui.run.main.variables.VariableDialog;
+import org.intellij.xquery.runner.ui.run.main.variables.VariableDialogWrapper;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * User: ligasgr
+ * Date: 19/11/13
+ * Time: 21:49
+ */
+public class VariablesPanel extends JPanel implements PanelWithAnchor {
+
+    private final ColumnInfo<XQueryRunVariable, String> NAME = new NameColumnInfo();
+    private final ColumnInfo<XQueryRunVariable, String> NAMESPACE = new NamespaceColumnInfo();
+    private final ColumnInfo<XQueryRunVariable, String> TYPE = new TypeColumnInfo();
+    private final ColumnInfo<XQueryRunVariable, String> VALUE = new ValueColumnInfo();
+    private final ColumnInfo<XQueryRunVariable, Boolean> IS_ACTIVE = new IsActiveColumnInfo();
+    private JComponent anchor;
+    private final TableView<XQueryRunVariable> variablesTable;
+    private final ListTableModel<XQueryRunVariable> variablesModel;
+
+    public VariablesPanel() {
+        super(new BorderLayout());
+        variablesModel = new ListTableModel<XQueryRunVariable>(IS_ACTIVE, NAME, NAMESPACE, TYPE, VALUE);
+        variablesTable = prepareVariablesTable();
+        ToolbarDecorator variablesTableToolbarDecorator = prepareVariablesTableToolbarDecorator(variablesTable);
+        add(variablesTableToolbarDecorator.createPanel());
+        setPreferredSize(new Dimension(- 1, 120));
+    }
+
+    private ToolbarDecorator prepareVariablesTableToolbarDecorator(final TableView<XQueryRunVariable> variablesTable) {
+        return ToolbarDecorator.createDecorator(variablesTable)
+                .setAddAction(new AnActionButtonRunnable() {
+                    @Override
+                    public void run(AnActionButton button) {
+                        ArrayList<XQueryRunVariable> newList = new ArrayList<XQueryRunVariable>
+                                (variablesModel.getItems());
+                        XQueryRunVariable newVariable = new XQueryRunVariable();
+                        if (showEditorDialog(newVariable)) {
+                            newList.add(newVariable);
+                            variablesModel.setItems(newList);
+                            int index = variablesModel.getRowCount() - 1;
+                            variablesModel.fireTableRowsInserted(index, index);
+                            variablesTable.setRowSelectionInterval(index, index);
+                        }
+                    }
+                }).setRemoveAction(new AnActionButtonRunnable() {
+                    @Override
+                    public void run(AnActionButton button) {
+                        TableUtil.stopEditing(variablesTable);
+                        final int[] selected = variablesTable.getSelectedRows();
+                        if (selected == null || selected.length == 0) return;
+                        for (int i = selected.length - 1; i >= 0; i--) {
+                            variablesModel.removeRow(selected[i]);
+                        }
+                        for (int i = selected.length - 1; i >= 0; i--) {
+                            int idx = selected[i];
+                            variablesModel.fireTableRowsDeleted(idx, idx);
+                        }
+                        int selection = selected[0];
+                        if (selection >= variablesModel.getRowCount()) {
+                            selection = variablesModel.getRowCount() - 1;
+                        }
+                        if (selection >= 0) {
+                            variablesTable.setRowSelectionInterval(selection, selection);
+                        }
+                        variablesTable.requestFocus();
+                    }
+                }).setEditAction(new AnActionButtonRunnable() {
+                    @Override
+                    public void run(AnActionButton button) {
+                        final int selectedRow = variablesTable.getSelectedRow();
+                        final XQueryRunVariable selectedVariable = variablesTable.getSelectedObject();
+                        showEditorDialog(selectedVariable);
+                        variablesModel.fireTableDataChanged();
+                        variablesTable.setRowSelectionInterval(selectedRow, selectedRow);
+                    }
+                }).setRemoveActionUpdater(new AnActionButtonUpdater() {
+                    @Override
+                    public boolean isEnabled(AnActionEvent e) {
+                        return variablesTable.getSelectedRowCount() >= 1;
+                    }
+                }).setEditActionUpdater(new AnActionButtonUpdater() {
+                    @Override
+                    public boolean isEnabled(AnActionEvent e) {
+                        return variablesTable.getSelectedRowCount() >= 1 &&
+                                variablesTable.getSelectedObject() != null;
+                    }
+                }).disableUpDownActions().initPosition();
+    }
+
+    private TableView<XQueryRunVariable> prepareVariablesTable() {
+        TableView<XQueryRunVariable> variablesTable = new TableView<XQueryRunVariable>(variablesModel);
+        variablesTable.getEmptyText().setText("No variables defined");
+        variablesTable.setColumnSelectionAllowed(false);
+        variablesTable.setShowGrid(false);
+        variablesTable.setDragEnabled(false);
+        variablesTable.setShowHorizontalLines(false);
+        variablesTable.setShowVerticalLines(false);
+        variablesTable.setIntercellSpacing(new Dimension(0, 0));
+        return variablesTable;
+    }
+
+    public void setVariables(XQueryRunVariables variables) {
+        ArrayList<XQueryRunVariable> newList = new ArrayList<XQueryRunVariable>(variables.getVariables());
+        variablesModel.setItems(newList);
+    }
+
+    private static boolean showEditorDialog(@NotNull XQueryRunVariable variable) {
+        VariableDialogWrapper wrapper = new VariableDialogWrapper(new VariableDialog());
+        wrapper.init(variable);
+        wrapper.show();
+        if (wrapper.isOK()) {
+            wrapper.applyValuesTo(variable);
+            return true;
+        }
+        return false;
+    }
+
+    public Collection<XQueryRunVariable> getVariables() {
+        return variablesModel.getItems();
+    }
+
+    @Override
+    public JComponent getAnchor() {
+        return anchor;
+    }
+
+    @Override
+    public void setAnchor(@Nullable JComponent anchor) {
+        this.anchor = anchor;
+    }
+}
+
