9921f32dfa273e08c7e6fd265914068e6fe8f337
ligasgr <ligasgr@gmail.com>
2014-01-01 21:56:00 +0000

    Update copyright NOTICE.

--- src/main/java/org/intellij/xquery/runner/state/run/VariablesAccessor.java
+++ src/main/java/org/intellij/xquery/runner/state/run/VariablesAccessor.java
@@ -1,5 +1,6 @@
 /*
- * Copyright 2013 Grzegorz Ligas <ligasgr@gmail.com> and other contributors (see the CONTRIBUTORS file).
+ * Copyright 2013-2014 Grzegorz Ligas <ligasgr@gmail.com> and other contributors
+ * (see the CONTRIBUTORS file).
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.intellij.xquery.runner.state.run;
 
 import com.intellij.util.xmlb.XmlSerializer;
