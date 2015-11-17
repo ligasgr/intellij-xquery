ff6593697ab38e02ff875c1d468ffae7d9b05de4
ligasgr <ligasgr@gmail.com>
2015-11-17 02:02:39 +0000

    Isses #123 - Add to list of MarkLogic extended syntax.

--- src/main/java/org/intellij/xquery/inspection/marklogic/MarklogicExtendedSyntaxInspection.java
+++ src/main/java/org/intellij/xquery/inspection/marklogic/MarklogicExtendedSyntaxInspection.java
@@ -24,20 +24,34 @@ import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.util.PsiTreeUtil;
 import org.intellij.xquery.psi.XQueryCatchClauseExpression;
 import org.intellij.xquery.psi.XQueryFile;
 import org.intellij.xquery.psi.XQueryMarklogicAnnotation;
+import org.intellij.xquery.psi.XQueryMarklogicAnyKindTest;
+import org.intellij.xquery.psi.XQueryMarklogicArrayNodeTest;
 import org.intellij.xquery.psi.XQueryMarklogicBinaryTest;
+import org.intellij.xquery.psi.XQueryMarklogicBooleanNodeTest;
 import org.intellij.xquery.psi.XQueryMarklogicCatchErrorList;
+import org.intellij.xquery.psi.XQueryMarklogicCompArrayNodeConstructor;
 import org.intellij.xquery.psi.XQueryMarklogicCompBinaryConstructor;
+import org.intellij.xquery.psi.XQueryMarklogicCompBooleanNodeConstructor;
+import org.intellij.xquery.psi.XQueryMarklogicCompNullNodeConstructor;
+import org.intellij.xquery.psi.XQueryMarklogicCompNumberNodeConstructor;
+import org.intellij.xquery.psi.XQueryMarklogicCompObjectNodeConstructor;
 import org.intellij.xquery.psi.XQueryMarklogicNamespaceAxis;
+import org.intellij.xquery.psi.XQueryMarklogicNullNodeTest;
+import org.intellij.xquery.psi.XQueryMarklogicNumberNodeTest;
+import org.intellij.xquery.psi.XQueryMarklogicObjectNodeTest;
+import org.intellij.xquery.psi.XQueryMarklogicTextTest;
 import org.intellij.xquery.psi.XQueryMarklogicValidation;
+import org.intellij.xquery.psi.XQueryPsiElement;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+
+import static com.intellij.psi.util.PsiTreeUtil.findChildrenOfType;
 
 public class MarklogicExtendedSyntaxInspection extends LocalInspectionTool {
 
@@ -54,41 +68,32 @@ public class MarklogicExtendedSyntaxInspection extends LocalInspectionTool {
     }
 
     private ProblemDescriptor[] findMarklogicExtendedSyntax(PsiFile file, InspectionManager manager) {
-        Collection<XQueryMarklogicAnnotation> marklogicAnnotations =
-                PsiTreeUtil.findChildrenOfType(file, XQueryMarklogicAnnotation.class);
-        Collection<XQueryMarklogicCatchErrorList> marklogicCatchErrorLists =
-                PsiTreeUtil.findChildrenOfType(file, XQueryMarklogicCatchErrorList.class);
-        Collection<XQueryMarklogicNamespaceAxis> marklogicNamespaceAxes =
-                PsiTreeUtil.findChildrenOfType(file, XQueryMarklogicNamespaceAxis.class);
-        Collection<XQueryMarklogicBinaryTest> marklogicBinaryTests =
-                PsiTreeUtil.findChildrenOfType(file, XQueryMarklogicBinaryTest.class);
-        Collection<XQueryMarklogicCompBinaryConstructor> marklogicBinaryConstructors =
-                PsiTreeUtil.findChildrenOfType(file, XQueryMarklogicCompBinaryConstructor.class);
-        Collection<XQueryMarklogicValidation> marklogicValidations =
-                PsiTreeUtil.findChildrenOfType(file, XQueryMarklogicValidation.class);
-        int size = marklogicAnnotations.size() + marklogicCatchErrorLists.size() + marklogicNamespaceAxes.size()
-                + marklogicBinaryTests.size() + marklogicBinaryConstructors.size() + marklogicValidations.size();
-        List<ProblemDescriptor> problems = new ArrayList<ProblemDescriptor>(size);
-        for (XQueryMarklogicAnnotation element : marklogicAnnotations) {
+        Collection<PsiElement> marklogicExtendedSyntaxElements = new ArrayList<PsiElement>();
+        addAllMarkLogicSpecifics(file, marklogicExtendedSyntaxElements, XQueryMarklogicAnnotation.class);
+        addAllMarkLogicSpecifics(file, marklogicExtendedSyntaxElements, XQueryMarklogicCatchErrorList.class);
+        addAllMarkLogicSpecifics(file, marklogicExtendedSyntaxElements, XQueryMarklogicNamespaceAxis.class);
+        addAllMarkLogicSpecifics(file, marklogicExtendedSyntaxElements, XQueryMarklogicBinaryTest.class);
+        addAllMarkLogicSpecifics(file, marklogicExtendedSyntaxElements, XQueryMarklogicCompBinaryConstructor.class);
+        addAllMarkLogicSpecifics(file, marklogicExtendedSyntaxElements, XQueryMarklogicValidation.class);
+        addAllMarkLogicSpecifics(file, marklogicExtendedSyntaxElements, XQueryMarklogicCompObjectNodeConstructor.class);
+        addAllMarkLogicSpecifics(file, marklogicExtendedSyntaxElements, XQueryMarklogicCompNumberNodeConstructor.class);
+        addAllMarkLogicSpecifics(file, marklogicExtendedSyntaxElements, XQueryMarklogicCompBooleanNodeConstructor.class);
+        addAllMarkLogicSpecifics(file, marklogicExtendedSyntaxElements, XQueryMarklogicCompNullNodeConstructor.class);
+        addAllMarkLogicSpecifics(file, marklogicExtendedSyntaxElements, XQueryMarklogicCompArrayNodeConstructor.class);
+        addAllMarkLogicSpecifics(file, marklogicExtendedSyntaxElements, XQueryMarklogicTextTest.class);
+        addAllMarkLogicSpecifics(file, marklogicExtendedSyntaxElements, XQueryMarklogicAnyKindTest.class);
+        addAllMarkLogicSpecifics(file, marklogicExtendedSyntaxElements, XQueryMarklogicObjectNodeTest.class);
+        addAllMarkLogicSpecifics(file, marklogicExtendedSyntaxElements, XQueryMarklogicNumberNodeTest.class);
+        addAllMarkLogicSpecifics(file, marklogicExtendedSyntaxElements, XQueryMarklogicBooleanNodeTest.class);
+        addAllMarkLogicSpecifics(file, marklogicExtendedSyntaxElements, XQueryMarklogicNullNodeTest.class);
+        addAllMarkLogicSpecifics(file, marklogicExtendedSyntaxElements, XQueryMarklogicArrayNodeTest.class);
+        List<ProblemDescriptor> problems = new ArrayList<ProblemDescriptor>(marklogicExtendedSyntaxElements.size());
+        for (PsiElement element : marklogicExtendedSyntaxElements) {
             problems.add(createProblem(manager, element));
         }
-        for (XQueryMarklogicCatchErrorList element : marklogicCatchErrorLists) {
-            problems.add(createProblem(manager, element));
-        }
-        for (XQueryMarklogicNamespaceAxis element : marklogicNamespaceAxes) {
-            problems.add(createProblem(manager, element));
-        }
-        for (XQueryMarklogicBinaryTest element : marklogicBinaryTests) {
-            problems.add(createProblem(manager, element));
-        }
-        for (XQueryMarklogicCompBinaryConstructor element : marklogicBinaryConstructors) {
-            problems.add(createProblem(manager, element));
-        }
-        for (XQueryMarklogicValidation element : marklogicValidations) {
-            problems.add(createProblem(manager, element));
-        }
+
         Collection<XQueryCatchClauseExpression> marklogicCatchClause =
-                PsiTreeUtil.findChildrenOfType(file, XQueryCatchClauseExpression.class);
+                findChildrenOfType(file, XQueryCatchClauseExpression.class);
 
         for (XQueryCatchClauseExpression element : marklogicCatchClause) {
             if (element.getExpr() == null) {
@@ -96,6 +101,11 @@ public class MarklogicExtendedSyntaxInspection extends LocalInspectionTool {
             }
         }
         return problems.toArray(new ProblemDescriptor[problems.size()]);
+    }
+
+    private boolean addAllMarkLogicSpecifics(PsiFile file, Collection<PsiElement> marklogicExtendedSyntaxElements,
+                                             Class<? extends XQueryPsiElement> aClass) {
+        return marklogicExtendedSyntaxElements.addAll(findChildrenOfType(file, aClass));
     }
 
     private ProblemDescriptor createProblem(InspectionManager manager, PsiElement element) {
